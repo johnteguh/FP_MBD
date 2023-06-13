31672f14ec1e426baf60df7462358f6d1ff2e8ca
johnteguh <yohanes.teguh36@gmail.com>
2023-06-14 01:24:34 +0700

    Update viewqr.sql

--- teguh/viewqr.sql
+++ teguh/viewqr.sql
@@ -1,10 +1,10 @@
 -- View untuk menampilkan banyak produk yang dibeli setiap transaksi
 
-CREATE VIEW View_Transaksi_Count AS
+CREATE VIEW Total_Pertransaksi AS
 SELECT tr.idtransaksi, COUNT(td.transaksi_IDTransaksi) AS jumlah_produk
 FROM DetailTransaksi td, Transaksi tr
 WHERE td.transaksi_IDTransaksi = tr.idtransaksi
 GROUP BY tr.idtransaksi
 ORDER BY tr.idtransaksi ASC;
 
-SELECT * FROM View_Transaksi_Count;
+SELECT * FROM Total_Pertransaksi;
