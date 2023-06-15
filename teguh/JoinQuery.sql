66e35e8d0c852253102883f03e3a53efccc33c7f
johnteguh <yohanes.teguh36@gmail.com>
2023-06-15 07:27:21 +0700

    Update JoinQuery.sql

---
+++ teguh/JoinQuery.sql
@@ -0,0 +1,10 @@
+
+-- menampilkan idtransaksinya dengan iddetailtransaksinya
+SELECT t.IDTransaksi, d.IDDetailTransaksi
+FROM Transaksi t
+JOIN DetailTransaksi d ON t.IDTransaksi = d.Transaksi_IDTransaksi
+ORDER BY t.idtransaksi;
+
+SELECT b.NAMA AS "Brand",p.NAMA AS "Produk" FROM PRODUK p
+INNER JOIN BRAND b ON p.BRAND_IDBRAND = b.IDBRAND
+WHERE p.NAMA LIKE '%SPF%' AND b.nama LIKE '%O%';
